3fc92b34846cafca5449160f837dc855b7bef675
unknown <Administrator@LAB2-PC11.labs.org>
2026-06-04 16:24:52 +0500

    first commit

---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use app\http\Controller\AuthController;
+use app\http\Controller\UserController;
+use app\http\Controller\adminController;
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
